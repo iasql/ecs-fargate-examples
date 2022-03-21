9368a6342b940b1dde05d2b4531d068a343b8813
Alejandro Guillen <aguillenv93@gmail.com>
2022-03-21 16:01:31 +0100

    Update settings

--- django/quickstart/app/settings.py
+++ django/quickstart/app/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from os import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -80,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'infra',
         'USER': 'postgres',
-        'PASSWORD': 'test',
+        'PASSWORD': environ.get('DB_PASSWORD'),
         'HOST': 'localhost',
         'PORT': '5432',
     }
