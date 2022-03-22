d8fe3963a341e5cb118446e4eba567caebbc6209
depombo <lfdepombo@gmail.com>
2022-03-22 08:53:32 -0400

    forgot semicolon

--- typeorm/infra/src/migration/1646683871211-Install.js
+++ typeorm/infra/src/migration/1646683871211-Install.js
@@ -3,7 +3,7 @@ module.exports = class Install1646683871211 {
   // make sure the correct iasql modules are installed or the tables won't exist
   async up(queryRunner) {
     await queryRunner.query(`
-      BEGIN
+      BEGIN;
         CALL iasql_install(array[
           'aws_vpc@0.0.1',
           'aws_security_group@0.0.1',
@@ -19,7 +19,7 @@ module.exports = class Install1646683871211 {
 
   async down(queryRunner) {
     await queryRunner.query(`
-      BEGIN
+      BEGIN;
         CALL iasql_uninstall(array[
           'aws_vpc@0.0.1',
           'aws_security_group@0.0.1',
