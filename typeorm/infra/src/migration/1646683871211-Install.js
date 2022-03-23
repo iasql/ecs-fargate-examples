7d090a8df1538cde945cae3091862cb169d22852
luisfer <lfdepombo@gmail.com>
2022-03-23 14:34:55 -0700

    add module installation typeorm migration (#16)

---
+++ typeorm/infra/src/migration/1646683871211-Install.js
@@ -0,0 +1,31 @@
+module.exports = class Install1646683871211 {
+
+  // make sure the correct iasql modules are installed or the tables won't exist
+  async up(queryRunner) {
+    await queryRunner.query(`
+      SELECT iasql_install(array[
+        'aws_vpc@0.0.1',
+        'aws_security_group@0.0.1',
+        'aws_elb@0.0.1',
+        'aws_cloudwatch@0.0.1',
+        'aws_ecr@0.0.1',
+        'aws_ecs_fargate@0.0.1',
+        'aws_rds@0.0.1'
+      ]);
+    `);
+  }
+
+  async down(queryRunner) {
+    await queryRunner.query(`
+      SELECT iasql_uninstall(array[
+        'aws_vpc@0.0.1',
+        'aws_security_group@0.0.1',
+        'aws_elb@0.0.1',
+        'aws_cloudwatch@0.0.1',
+        'aws_ecr@0.0.1',
+        'aws_ecs_fargate@0.0.1',
+        'aws_rds@0.0.1'
+      ]);
+    `);
+  }
+}
