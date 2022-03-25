144d105d974ab35829f6a16e373de327ae127f9b
David Ellis <isv.damocles@gmail.com>
2022-03-25 09:36:23 -0500

    Also use the variadic install and uninstall functions here

--- typeorm/infra/src/migration/1646683871211-Install.js
+++ typeorm/infra/src/migration/1646683871211-Install.js
@@ -3,7 +3,7 @@ module.exports = class Install1646683871211 {
   // make sure the correct iasql modules are installed or the tables won't exist
   async up(queryRunner) {
     await queryRunner.query(`
-      SELECT iasql_install(array[
+      SELECT iasql_install(
         'aws_vpc@0.0.1',
         'aws_security_group@0.0.1',
         'aws_elb@0.0.1',
@@ -11,13 +11,13 @@ module.exports = class Install1646683871211 {
         'aws_ecr@0.0.1',
         'aws_ecs_fargate@0.0.1',
         'aws_rds@0.0.1'
-      ]);
+      );
     `);
   }
 
   async down(queryRunner) {
     await queryRunner.query(`
-      SELECT iasql_uninstall(array[
+      SELECT iasql_uninstall(
         'aws_vpc@0.0.1',
         'aws_security_group@0.0.1',
         'aws_elb@0.0.1',
@@ -25,7 +25,7 @@ module.exports = class Install1646683871211 {
         'aws_ecr@0.0.1',
         'aws_ecs_fargate@0.0.1',
         'aws_rds@0.0.1'
-      ]);
+      );
     `);
   }
 }
