9f95eb93943eedcea790dce8a5f8b1739e383f6b
Alejandro Guillen <aguillenv93@gmail.com>
2022-05-31 16:31:09 +0200

    Update install

--- typeorm/infra/src/migration/1646683871211-Install.js
+++ typeorm/infra/src/migration/1646683871211-Install.js
@@ -4,14 +4,7 @@ module.exports = class Install1646683871211 {
   async up(queryRunner) {
     await queryRunner.query(`
       SELECT * FROM iasql_install(
-        'aws_iam',
-        'aws_vpc',
-        'aws_security_group',
-        'aws_elb',
-        'aws_cloudwatch',
-        'aws_ecr',
-        'aws_ecs_fargate',
-        'aws_rds'
+        'aws_ecs_simplified'
       );
     `);
   }
@@ -19,14 +12,7 @@ module.exports = class Install1646683871211 {
   async down(queryRunner) {
     await queryRunner.query(`
       SELECT * FROM iasql_uninstall(
-        'aws_iam',
-        'aws_vpc',
-        'aws_security_group',
-        'aws_elb',
-        'aws_cloudwatch',
-        'aws_ecr',
-        'aws_ecs_fargate',
-        'aws_rds'
+        'aws_ecs_simplified'
       );
     `);
   }
