fd984ca92714cb53f1128a1a3e7cef34025d559f
Alejandro Guillen <aguillenv93@gmail.com>
2022-06-30 16:29:25 +0200

    Set policy as json instead of string (#34)

--- prisma/infra/index.js
+++ prisma/infra/index.js
@@ -67,7 +67,7 @@ async function main() {
   const role = await prisma.role.create({
     data: {
       role_name: `${RUN_ID}_ecsTaskExecRole_${REGION}`,
-      assume_role_policy_document: '{"Version":"2012-10-17","Statement":[{"Sid":"","Effect":"Allow","Principal":{"Service":"ecs-tasks.amazonaws.com"},"Action":"sts:AssumeRole"}]}',
+      assume_role_policy_document: {"Version":"2012-10-17","Statement":[{"Sid":"","Effect":"Allow","Principal":{"Service":"ecs-tasks.amazonaws.com"},"Action":"sts:AssumeRole"}]},
       attached_policies_arns: ['arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy']
     }
   });
