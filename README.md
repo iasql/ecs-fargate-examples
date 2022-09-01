730160aa12cbb530a2f8146a7152b363bc1df01f
Luis Fernando <lfdepombo@gmail.com>
2022-09-01 12:20:45 -0400

    get rid of flyway, migrations and typeorm and use `ecs_simplified` with prisma (#38)
    * get rid of flyway and use ecs_simplified in prisma
    
    * add image tag and fix typo
    
    * keep run id
    
    * oxford comma
    
    * define RUN_ID
    
    * reorder variables
    
    * use app name variable name
    
    * get rid of TypeORM
    
    * drop RUN_ID

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # ECS Fargate Examples with IaSQL on different ORMs
 
-This [template repository](https://docs.github.com/en/repositories/creating-and-managing-repositories/creating-a-repository-from-a-template) houses quickstart examples of how to use IaSQL with different ORMs to deploy an HTTP server within a docker container on your AWS account using Fargate ECS, IAM, ECR and ELB. The container image will be hosted as a private repository in ECR and deployed to ECS using Fargate.
+This [template repository](https://docs.github.com/en/repositories/creating-and-managing-repositories/creating-a-repository-from-a-template) houses quickstart examples of how to use IaSQL with different ORMs to deploy an HTTP server within a docker container on your AWS account using Fargate ECS, IAM, ECR, and ELB. The container image will be hosted as a private repository in ECR and deployed to ECS using Fargate.
 
 To illustrate all the different ways in which infrastructure can be managed with IaSQL, we implement the same infrastructure with IaSQL using different ORMs. Each of them points to their respective tutorial:
-- [TypeORM (SQL ORM)](https://docs.iasql.com/typeorm/)
-- [Flyway (SQL)](https://docs.iasql.com/flyway/)
 - [Prisma (Javascript)](https://docs.iasql.com/prisma/)
 - [Django (Python)](https://docs.iasql.com/django/)
+
+The pure SQL tutorial lives [here](https://docs.iasql.com/sql/)
--- prisma/infra/index.js
+++ prisma/infra/index.js
@@ -1,146 +1,48 @@
 const { execSync } = require('child_process')
-const { PrismaClient, load_balancer_scheme_enum, task_definition_cpu_memory_enum } = require('@prisma/client');
+const { PrismaClient } = require('@prisma/client');
 
 const pkg = require('./package.json');
-// TODO replace with your desired project name
-const PROJECT_NAME = pkg.name;
 
 const REGION = process.env.AWS_REGION ?? '';
-const RUN_ID = process.env.RUN_ID ?? '';
-const CONTAINER_MEM_RESERVATION = 8192; // in MiB
 const PORT = 8088;
+
+// TODO replace with your desired project name
+const APP_NAME = pkg.name;
 
 const prisma = new PrismaClient()
 
-// TODO `prisma generate` using camelcase
-// TODO use Redwood JS data migrations https://redwoodjs.com/docs/data-migrations
-// or add uniqueness constraints and use upserts to make the script idempotent
 async function main() {
-  /* AWS SECURITY GROUPS */
-  const sg = await prisma.security_group.create({
-    data: {
-      description: `${PROJECT_NAME} security group`, group_name: `${PROJECT_NAME}-security-group`,
-    },
-  });
-  // TODO replace cidr with string or prisma to support cidr so we can use entity
-  await prisma.$executeRawUnsafe(`
-    INSERT INTO security_group_rule (is_egress, ip_protocol, from_port, to_port, cidr_ipv4, description, security_group_id)
-    VALUES (false, 'tcp', ${PORT}, ${PORT}, '0.0.0.0/0', '${PROJECT_NAME}-security-group', ${sg.id});
-  `);
-  await prisma.$executeRawUnsafe(`
-    INSERT INTO security_group_rule (is_egress, ip_protocol, from_port, to_port, cidr_ipv4, description, security_group_id)
-    VALUES (true, '-1', -1, -1, '0.0.0.0/0', '${PROJECT_NAME}-security-group', ${sg.id});
-  `);
-
-  // AWS ELASTIC LOAD BALANCER
-  const tg = await prisma.target_group.create({
-    data: {
-      target_group_name: `${PROJECT_NAME}-target`,
-      target_type: 'ip',
-      protocol: 'HTTP',
-      port: PORT,
-      health_check_path: '/health'
-    },
-  });
-  const lb = await prisma.load_balancer.create({
-    data: {
-      load_balancer_name: `${PROJECT_NAME}-load-balancer`,
-      scheme: load_balancer_scheme_enum.internet_facing,
-      load_balancer_type: 'application',
-      ip_address_type: 'ipv4',
-      load_balancer_security_groups: {
-        create: {
-          security_group_id: sg.id,
-        }
-      }
-    }
-  });
-  const listener = await prisma.listener.create({
-    data: {
-      load_balancer_name: lb.load_balancer_name,
-      port: PORT,
-      protocol: 'HTTP',
-      action_type: 'forward',
-      target_group_name: tg.target_group_name,
-    }
-  });
-  const role = await prisma.role.create({
-    data: {
-      role_name: `${RUN_ID}_ecsTaskExecRole_${REGION}`,
-      assume_role_policy_document: {"Version":"2012-10-17","Statement":[{"Sid":"","Effect":"Allow","Principal":{"Service":"ecs-tasks.amazonaws.com"},"Action":"sts:AssumeRole"}]},
-      attached_policies_arns: ['arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy']
-    }
-  });
-  const repo = await prisma.repository.create({
-    data: { repository_name: `${PROJECT_NAME}-repository`}
-  });
-  const logGroup = await prisma.log_group.create({
-    data: { log_group_name: `${PROJECT_NAME}-log-group`}
-  });
-  const cluster = await prisma.cluster.create({
-    data: { cluster_name: `${PROJECT_NAME}-cluster`}
-  });
-  // https://docs.aws.amazon.com/AmazonECS/latest/developerguide/task_definition_parameters.html
-  const task = await prisma.task_definition.create({
-    data: {
-      family: `${PROJECT_NAME}-td`, cpu_memory: task_definition_cpu_memory_enum.vCPU2_8GB,
-      task_role_name: role.role_name, execution_role_name: role.role_name,
-    }
-  });
-  const container = await prisma.container_definition.create({
-    data: {
-      name: `${PROJECT_NAME}-container`, essential: true,
-      log_group_name: logGroup.log_group_name,
-      repository_name: repo.repository_name, task_definition_id: task.id, tag: 'latest',
-      memory_reservation: CONTAINER_MEM_RESERVATION,
-      host_port: PORT, container_port: PORT, protocol: 'tcp',
-    }
-  });
-  // select array(select subnet_id from subnet inner join vpc on vpc.id = subnet.vpc_id where is_default = true limit 3),
-  const vpc_res = await prisma.vpc.findFirst({
-    where: { is_default: true },
-    select: {
-      subnet: {
-        select: {
-          subnet_id: true
-        }
-      }
-    }
-  });
-  const subnet_ids = vpc_res.subnet.map(s => s.subnet_id);
-  // create ECS service and associate it to security group
-  const service = await prisma.service.create({
-    data: {
-      name: `${PROJECT_NAME}-service`, desired_count: 1, assign_public_ip: 'ENABLED',
-      cluster_name: cluster.cluster_name, task_definition_id: task.id, target_group_name: tg.target_group_name,
-      subnets: subnet_ids,
-      service_security_groups: {
-        create: {
-          security_group_id: sg.id,
-        }
-      }
-    }
+  const data = {
+    app_name: APP_NAME,
+    public_ip: true,
+    app_port: PORT,
+    image_tag: 'latest'
+  };
+  await prisma.ecs_simplified.upsert({
+    where: { app_name: APP_NAME},
+    create: data,
+    update: data,
   });
 
   const apply = await prisma.$queryRaw`SELECT * from iasql_apply();`
   console.dir(apply)
 
-  const repo_uri = (await prisma.repository.findFirst({
-    where: { repository_name: `${PROJECT_NAME}-repository`},
+  const repoUri = (await prisma.ecs_simplified.findFirst({
+    where: { app_name: APP_NAME },
     select: { repository_uri: true }
   })).repository_uri;
 
   console.log('Docker login...')
-  execSync(`aws ecr get-login-password --region ${REGION} | docker login --username AWS --password-stdin ${repo_uri}`)
+  execSync(`aws ecr get-login-password --region ${REGION} | docker login --username AWS --password-stdin ${repoUri}`)
 
   console.log('Building image...')
-  execSync(`docker build -t ${PROJECT_NAME}-repository ${__dirname}/../app`);
+  execSync(`docker build -t ${APP_NAME}-repository ${__dirname}/../app`);
 
   console.log('Tagging image...')
-  execSync(`docker tag ${PROJECT_NAME}-repository:latest ${repo_uri}:latest`);
+  execSync(`docker tag ${APP_NAME}-repository:latest ${repoUri}:latest`);
 
   console.log('Pushing image...')
-  execSync(`docker push ${repo_uri}:latest`);
+  execSync(`docker push ${repoUri}:latest`);
 }
 
 main()
